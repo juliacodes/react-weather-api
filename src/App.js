4fac7c3938b144cae5763f73998dca889d1b4448
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-06-29 17:25:14 +0200

    Replaced all data with data from new api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
-
 import styled from "styled-components";
 
 const AppWrapper = styled.div``;
@@ -12,37 +11,72 @@ const API_KEY = "b8d5d166fa637e392446c7de40113758";
 
 class App extends React.Component {
   state = {
-    temperature: undefined,
-    // city: undefined,
-    // country: undefined,
-    // humidity: undefined,
-    // description: undefined,
+    date: undefined,
+    city: undefined,
+    //current
+    temperatureNow: undefined,
+    temperatureNowHigh: undefined,
+    conditionsToday: undefined,
+    windSpeed: undefined,
+    // tomorrow
+    temperatureTomorrow: undefined,
+    conditionsTomorrow: undefined,
+    // day 3
+    temperatureDay3: undefined,
+    conditionsDay3: undefined,
+    // day 4
+    temperatureDay4: undefined,
+    conditionsDay4: undefined,
+    // day 5
+    temperatureDay5: undefined,
+    conditionsDay5: undefined,
+    // error
     error: undefined
   };
   getWeather = async e => {
     e.preventDefault();
     const zip = e.target.elements.zip.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${API_KEY}&units=imperial`
     );
     const list = await api_call.json();
 
-    // if (cityCode) {
-    console.log(list);
+    if (zip) {
+      console.log(list);
 
-    this.setState({
-      temperature: list.city.name
-      // city: data.name,
-      // country: data.sys.country,
-      // humidity: data.main.humidity,
-      // description: data.weather[0].description, // selects description within array number 1
-      // error: ""
-    });
-    // } else {
-    // this.setState({
-    //   error: "Enter a city and country"
-    // });
-    // }
+      this.setState({
+        date: list.list[0].dt_txt,
+        zip: zip,
+        city: list.city.name,
+        //  current
+        temperatureNow: list.list[0].main.temp,
+        temperatureNowHigh: list.list[0].main.temp_max,
+        conditionsToday: list.list[0].weather[0].description,
+        windSpeed: list.list[0].wind.speed,
+
+        //  tomorrow
+        temperatureTomorrow: list.list[7].main.temp,
+        conditionsTomorrow: list.list[7].weather[0].description,
+
+        // day 3
+        temperatureDay3: list.list[15].main.temp,
+        conditionsDay3: list.list[15].weather[0].description,
+
+        // day 4
+        temperatureDay4: list.list[23].main.temp,
+        conditionsDay4: list.list[23].weather[0].description,
+
+        // day 5
+        temperatureDay5: list.list[23].main.temp,
+        conditionsDay5: list.list[23].weather[0].description,
+
+        error: ""
+      });
+    } else {
+      this.setState({
+        error: "Enter a city and country" // error message
+      });
+    }
   };
   render() {
     return (
@@ -50,11 +84,28 @@ class App extends React.Component {
         <Titles />
         <Form getWeather={this.getWeather} />
         <Weather
-          temperature={this.state.temperature}
+          // props to pass for all weather information
+          date={this.state.date}
+          zip={this.state.zip}
           city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
+          // current
+          temperatureNow={this.state.temperatureNow}
+          temperatureNowHigh={this.state.temperatureNowHigh}
+          conditionsToday={this.state.conditionsToday}
+          windSpeed={this.state.windSpeed}
+          // tomorrow
+          temperatureTomorrow={this.state.temperatureTomorrow}
+          conditionsTomorrow={this.state.conditionsTomorrow}
+          // day 3
+          temperatureDay3={this.state.temperatureDay3}
+          conditionsDay3={this.state.conditionsDay3}
+          // day 4
+          temperatureDay4={this.state.temperatureDay4}
+          conditionsDay4={this.state.conditionsDay4}
+          // day 5
+          temperatureDay5={this.state.temperatureDay5}
+          conditionsDay5={this.state.conditionsDay5}
+          // error
           error={this.state.error}
         />
       </AppWrapper>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const Today = styled.div``;
+const Future = styled.div``;
+const FutureDiv = styled.div``;
 
 const Weather = props => (
   <div>
-    {/* {props.city && props.country && ( */}
-    <div>
-      <p>
-        Location: {props.city}, {props.country}
-      </p>
-      <p>Temperature: {props.temperature}</p>
-      <p>Humidity: {props.humidity}</p>
-      <p>Conditions: {props.description}</p>
-    </div>
-    {/* )} */}
-
+    {props.zip && (
+      <div>
+        <Today>
+          <p>Date: {props.date}</p>
+          <p>Location: {props.city}</p>
+          <p>Temperature: {props.temperatureNow}</p>
+          <p>Description: {props.conditionsToday}</p>
+          <p>High: {props.temperatureNowHigh}</p>
+          <p>Wind Speed: {props.windSpeed}</p>
+        </Today>
+        <Future>
+          <FutureDiv>
+            <p>Today</p>
+            <p>Temperature: {props.temperatureNow}</p>
+            <p>Description: {props.conditionsToday}</p>
+          </FutureDiv>
+          <FutureDiv>
+            <p>Tomorrow</p>
+            <p>Temperature: {props.temperatureTomorrow}</p>
+            <p>Description: {props.conditionsTomorrow}</p>
+          </FutureDiv>
+          <FutureDiv>
+            <p>Day 3</p>
+            <p>Temperature: {props.temperatureDay3}</p>
+            <p>Description: {props.conditionsDay3}</p>
+          </FutureDiv>
+          <FutureDiv>
+            <p>Day 4</p>
+            <p>Temperature: {props.temperatureDay4}</p>
+            <p>Description: {props.conditionsDay4}</p>
+          </FutureDiv>
+          <FutureDiv>
+            <p>Day 5</p>
+            <p>Temperature: {props.temperatureDay5}</p>
+            <p>Description: {props.conditionsDay5}</p>
+          </FutureDiv>
+        </Future>
+      </div>
+    )}
     {props.error && <p>Error: {props.error}</p>}
   </div>
 );
